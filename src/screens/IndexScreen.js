ab470fac0e0f154516ceb932cf8710746c225093
Allina <allina606@gmail.com>
2020-04-23 17:34:43 -0400

    addBlogPosts function created- will be replaced w reducer

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,15 @@
 import React, { useContext } from 'react'
 import BlogContext from '../context/BlogContext'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native'
 
 const IndexScreen = () => {
-    const blogPosts = useContext(BlogContext)
+    const { data, addBlogPost } = useContext(BlogContext)
 
     return <View>
         <Text>Index Screen</Text>
+        <Button title="Add Post" onPress={addBlogPost} />
         <FlatList
-            data={blogPosts}
+            data={data}
             keyExtractor={(blogPost) => blogPost.title}
             renderItem = {({ item }) => {
                 return <Text>{item.title}</Text>
