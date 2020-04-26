a94ab86a383bd68fd43aafd06a6143a4ee2d7565
Allina <allina606@gmail.com>
2020-04-26 01:46:42 -0400

    delete and edit request to json server

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -13,19 +13,30 @@ const addBlogPost = dispatch => {
     return async (title, content, callback) => {
         await jsonServer.post('/blogposts', { title, content })
 
+        /* 
+            why is making a getBlogPosts() request in index.js from
+            navigation more fool proof than doing get request and 
+            dispatch from here? because it's not API addBlogPost
+            function dependent, always calls get from index.js page 
+        */
+
         if (callback) callback()
     }
 }
 
 const deleteBlogPost = dispatch => {
-    return (id) => {
+    return async (id) => {
+        await jsonServer.delete(`/blogposts/${id}`)
+
         dispatch({ type: 'delete_blogpost', payload: id })
     }
 }
 
 const editBlogPost = dispatch => {
-    return (id, title, content, callback) => {
-        dispatch({ type: 'edit_blogpost', payload: {title, content, id} })
+    return async (id, title, content, callback) => {
+            await jsonServer.put(`/blogposts/${id}`, { title, content })
+        
+        dispatch({ type: 'edit_blogpost', payload: { id, title, content }})
         if (callback) callback()
     }
 }
@@ -34,15 +45,6 @@ const editBlogPost = dispatch => {
 // reducer func what new state should be
 const blogReducer = ( state, action ) => {
     switch(action.type) {
-        case 'add_blogpost':
-            return [
-                ...state,
-                { 
-                    id: Math.floor(Math.random()*99999),
-                    title: action.payload.title,
-                    content: action.payload.content 
-                }
-            ]
         case 'delete_blogpost':
             // returns state datatype where all values == true to given condition, hence deleting current id from state
             return state.filter((blogPost) => blogPost.id !== action.payload)
